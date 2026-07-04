a397c32ee8be80f0506ab27be5088e63f12a3e6f
Bryan Kenth Estoesta <bryanestoesta2@gmail.com>
2026-07-04 23:09:20 +0800

    Stage 3 fix

--- lib/main.dart
+++ lib/main.dart
@@ -44,34 +44,39 @@ class _CounterScreenState extends State<CounterScreen> {
       backgroundColor: Colors.deepPurple.shade50,
       appBar: AppBar(
         title: const Text('My First Flutter App'),
-      ), // AppBar
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pressed the button this many times:',
-            ), // Text
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+      ),
+            body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'You have pressed the button this many times:',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  '$_counter',
+                  style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            const Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    'Created by: Bryan Kenth',
-                    style: TextStyle(
-                    fontSize: 14,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.deepPurple,
-                  ), 
+          ),
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                'Created by: Bryan Kenth',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.deepPurple,
                 ), 
               ), 
             ), 
-          ]
-        ),
+          ), 
+        ],
       ),
       floatingActionButton: Column(
         mainAxisAlignment:  MainAxisAlignment.end,
